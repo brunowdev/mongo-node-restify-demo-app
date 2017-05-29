b797c6409a3cfe3f94d72eead8895280dece6c16
Bruno Bitencourt Luiz <brunowdev@gmail.com>
2017-05-28 23:05:50 -0300

    configuração dinâmica de conexão no MongoDB

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ module.exports = {
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3000,
     db: {
-        uri: 'mongodb://$USERNAME:$PASSWORD@brunowdev-shard-00-00-b4idp.mongodb.net:27017,brunowdev-shard-00-01-b4idp.mongodb.net:27017,brunowdev-shard-00-02-b4idp.mongodb.net:27017/<DATABASE>?ssl=true&replicaSet=brunowdev-shard-0&authSource=admin' 
+        uri: 'mongodb://' + process.env['DB_USERNAME'].trim() + ':' + process.env['DB_PASSWORD'].trim() + '@brunowdev-shard-00-00-b4idp.mongodb.net:27017,brunowdev-shard-00-01-b4idp.mongodb.net:27017,brunowdev-shard-00-02-b4idp.mongodb.net:27017/<DATABASE>?ssl=true&replicaSet=brunowdev-shard-0&authSource=admin'
     }
 }
--- index.js
+++ index.js
@@ -1,43 +1,45 @@
-'use strict'
+'use strict';
 /**
  * Module Dependencies
  */
 const config  = require('./config'),
       restify = require('restify'),
-      mongodb = require('mongodb').MongoClient
+      mongodb = require('mongodb').MongoClient;
 /**
  * Initialize Server
  */
 const server = restify.createServer({
     name    : config.name,
     version : config.version
-})
+});
 
 /**
  * Bundled Plugins (http://restify.com/#bundled-plugins)
  */
-server.use(restify.jsonBodyParser({ mapParams: true }))
-server.use(restify.acceptParser(server.acceptable))
-server.use(restify.queryParser({ mapParams: true }))
-server.use(restify.fullResponse())
+server.use(restify.jsonBodyParser({ mapParams: true }));
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser({ mapParams: true }));
+server.use(restify.fullResponse());
 
 /**
  * Lift Server, Connect to DB & Require Route File
  */
 server.listen(config.port, () => {
+
     // establish connection to mongodb atlas
     mongodb.connect(config.db.uri, (err, db) => {
+        
         if (err) {
-            console.log('An error occurred while attempting to connect to MongoDB', err);
+            console.log('Ocorreu um erro ao se conectar com o MongoDB.', err);
             process.exit(1);
         }
         console.log(
-            '%s v%s ready to accept connections on port %s in %s environment.',
+            '%s v%s pronto para receber conexões na porta %s no ambiente %s.',
             server.name,
             config.version,
             config.port,
             config.env
-        )
+        );
         require('./routes')({ db, server })
-    })
-})
+    });
+});
